d21dcfa8ce613e216e0e2533ccda16f635f3b672
Aayush501 <shrivastavashu501@gmail.com>
2025-03-05 13:30:39 +0530

    [Aayush Kumar Shrivastav] Add . UC2 -> Solution

--- AddressBook.js
+++ AddressBook.js
@@ -11,4 +11,34 @@ class Contact {
     }
 }
 
-let addressBook = [];
+const addressBook = [];
+
+function validate(Contact) {
+    const nameRegex = /^[A-Z][a-z]{3,}/;
+    const addressRegex = /^.{4,}/;
+    const zipRegex = /^\d{6}$/;
+    const phoneRegex = /^\d{10}$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (!nameRegex.test(Contact.firstName) || !nameRegex.test(Contact.lastName)) {
+        throw "Name Not Valid !!!";
+    }
+
+    if (!addressRegex.test(Contact.address) || !addressRegex.test(Contact.city) || !addressRegex.test(Contact.state)) {
+        throw "Address Not Valid";
+    }
+
+    if (!zipRegex.test(Contact.zip)) {
+        throw "Invalid Contact Information";
+    }
+    
+    if( !phoneRegex.test(Contact.phoneNumber) ){
+        throw "Invalid Contact Information2";
+    }
+    
+    if(!emailRegex.test(Contact.email)){
+        throw "Invalid Contact Information3";
+    }
+    
+    return true;
+}
